ccd3fbe1e3d1ccca73bf70d85ce673701e562b27
Aparicio, Ernie <ernesto.aparicio@optum.com>
2020-06-22 10:32:09 -0700

    set.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,3 +1,4 @@
+import collections
 from collections import abc
 
 my_dict = {}
@@ -10,3 +11,38 @@ dial_codes = [(880, 'Bangladesh'), (55, 'Brazil')]
 
 # dict comprehension by looping and tuple unpacking
 country_dial = {country: code for code, country in dial_codes}
+
+# get values, set default if key missing
+print(country_dial.get(3, {'country': 'USA'}))
+
+# setDefault example
+index = {}
+
+for x in range(10):
+    index.setdefault(x, []).append(f'{x} index')
+
+print(index)
+
+# alternative to setDefault
+key = 0
+if key not in index:
+    index[key] = []
+    index[key].append(f'{x} index')
+
+print(index)
+
+# using defaultdict to set default empty list and avoid KeyError
+
+index = collections.defaultdict(list)
+for x in range(10):
+    index[x].append(f'{x} index')
+
+print(index)
+
+# using defaultdict to set default empty tuple and avoid KeyError
+
+index = collections.defaultdict(tuple)
+for x in range(10):
+    index[x] = (f'{x} index')
+
+print(len(index))
---
+++ set.py
@@ -0,0 +1,7 @@
+# great for membership tests, extremely fast
+
+a = set([1, 2, 3, 4, 4])
+# or
+#a = {1, 2, 4, 5, 6, '9'}
+# a.add('1')
+print(a)
